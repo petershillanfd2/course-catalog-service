58747e75e53b51722a214cc56bbc490b46e1ca0e
petershillanfd2 <georgepeter.shillan@fanduel.com>
2024-01-12 11:06:59 +0000

    Testing Multiple Sets of Data using @ParameterizedTest

--- src/intTest/kotlin/com/kotlinspring/coursecatalogservice/repository/CourseRepositoryIntTest.kt
+++ src/intTest/kotlin/com/kotlinspring/coursecatalogservice/repository/CourseRepositoryIntTest.kt
@@ -5,9 +5,13 @@ import com.kotlinspring.coursecatalogservice.util.courseList
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -34,5 +38,24 @@ class CourseRepositoryIntTest(@Autowired val courseRepository: CourseRepository)
         assertThat(results)
             .map<String>(Course::name)
             .contains("Starting SpringBoot 1", "Mastering SpringBoot 1")
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findByName_approach2(name: String, expectedSize: Int) {
+        val results = courseRepository.findByName(name)
+
+        assertThat(results)
+            .hasSize(expectedSize)
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Starting", 4)
+            )
+        }
     }
 }
